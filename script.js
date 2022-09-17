68c5d26a6513655a640d188cf1a7c3839205cb4a
Gonza <quin.gds@gmail.com>
2022-09-17 01:06:00 -0300

    limpieza codigo

--- script.js
+++ script.js
@@ -372,30 +372,3 @@ const btnCheckout =
 
 
 
-
-// swalWithBootstrapButtons.fire({
-//     title: 'Are you sure?',
-//     text: "You won't be able to revert this!",
-//     icon: 'warning',
-//     showCancelButton: true,
-//     confirmButtonText: 'Yes, delete it!',
-//     cancelButtonText: 'No, cancel!',
-//     reverseButtons: true
-// }).then((result) => {
-//     if (result.isConfirmed) {
-//         swalWithBootstrapButtons.fire(
-//             'Deleted!',
-//             'Your file has been deleted.',
-//             'success'
-//         )
-//     } else if (
-//         /* Read more about handling dismissals below */
-//         result.dismiss === Swal.DismissReason.cancel
-//     ) {
-//         swalWithBootstrapButtons.fire(
-//             'Cancelled',
-//             'Your imaginary file is safe :)',
-//             'error'
-//         )
-//     }
-// })
